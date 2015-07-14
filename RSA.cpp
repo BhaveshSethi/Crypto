cbabc724b3ed3a0d567516b83bfdd1624108d361
BhaveshSethi <bhaveshsethi1611@gmail.com>
2015-07-14 18:27:48 +0530

    initial commit

--- RSA.cpp
+++ RSA.cpp
@@ -1,1 +1,21 @@
 #include<iostream.h>
+#include<conio.h>
+
+int power(long long a, long long b, int c)
+{
+	int r = 1;
+	while(b>0)
+	{
+		if(b>0)
+			r = (r*a)%c;
+		b>>=1;
+		a = (a*a)%c;
+	}
+	return r;
+}
+
+
+void main()
+{
+
+}

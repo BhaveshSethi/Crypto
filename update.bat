62e3e74d117e7dfc7cf1e573c3e4a61f66d20f74
BhaveshSethi <bhaveshsethi1611@gmail.com>
2015-07-15 17:43:08 +0530

    twofish

--- update.bat
+++ update.bat
@@ -2,6 +2,7 @@ cp ../blowfish.cpp blowfish.cpp
 cp ../Des.cpp Des.cpp
 cp ../AES.cpp AES.cpp
 cp ../RSA.cpp RSA.cpp
+cp ../twofish.cpp  twofish.cpp
 git add .
 git commit -m "$1"
 git push -u origin master

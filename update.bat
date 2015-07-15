3bc21ad53279ce8acdfc16744dae5c53dd59e73d
BhaveshSethi <bhaveshsethi1611@gmail.com>
2015-07-16 01:05:19 +0530

    RC6

--- update.bat
+++ update.bat
@@ -2,7 +2,7 @@ cp ../blowfish.cpp blowfish.cpp
 cp ../Des.cpp Des.cpp
 cp ../AES.cpp AES.cpp
 cp ../RSA.cpp RSA.cpp
-cp ../twofish.cpp  twofish.cpp
+cp ../RC6.cpp RC6.cpp
 git add .
 git commit -m "$1"
 git push -u origin master

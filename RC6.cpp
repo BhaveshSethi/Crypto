9321e0a4265cacfc2931746b78b2132bb4751884
BhaveshSethi <bhaveshsethi1611@gmail.com>
2015-07-16 14:52:13 +0530

    almost

--- RC6.cpp
+++ RC6.cpp
@@ -39,7 +39,7 @@ void keySched(char k[16], unsigned long S[2*r+4])
 	}
 }
 
-void RC6(char C[16], char M[16], unsigned long S[2*r+4])
+void RC6Encrypt(char C[16], char M[16], unsigned long S[2*r+4])
 {
 	unsigned long A[4],t,u;
 	int i,j;
@@ -71,6 +71,52 @@ void RC6(char C[16], char M[16], unsigned long S[2*r+4])
 		A[i/4]>>=8;
 	}
 }
+void RC6Decrypt(char C[16], char M[16], unsigned long S[2*r+4])
+{
+	unsigned long A[4],t,u;
+	int i,j;
+	for(i=0;i<4;i++)
+	{
+		A[i]=0;
+		for(j=0;j<4;j++)
+			A[i] += (unsigned long)((int)C[4*i+j])<<(8*j);
+	}
+	A[2]-=S[2*r + 3];
+	if(A[2]<0)
+		A[2]+=0xffffffff;
+	A[0]-=S[2*r + 2];
+	if(A[0]<0)
+		A[0]+=0xffffffff;
+	for(i=r;i>=1;i--)
+	{
+		t=A[3];
+		A[3]=A[0];
+		A[0]=A[1];
+		A[1]=A[2];
+		A[2]=t;
+		u = Rol(A[3]*(A[3]<<1 + 1),5);
+		t = Rol(A[1]*(A[1]<<1 + 1),5);
+		A[2] = A[2] - S[2*i + 1];
+		if(A[2]<0)
+			A[2]+=0xffffffff;
+		A[2] = Ror(A[2],t&31) ^ u;
+		A[0] = A[0] - S[2*i];
+		if(A[0]<0)
+			A[0]+=0xffffffff;
+		A[0] = Ror(A[0],u&31) ^ t;
+	}
+	A[3] = A[3] - S[1];
+	if(A[3]<0)
+		A[3]+=0xffffffff;
+	A[1] = A[1] - S[0];
+	if(A[1]<0)
+		A[1]+=0xffffffff;
+	for(i=0;i<16;i++)
+	{
+		M[i] = (unsigned char)A[i/4];
+		A[i/4]>>=8;
+	}
+}
 
 void main()
 {
@@ -82,15 +128,23 @@ void main()
 	strcpy(key,"0123456789012345");
 	memset(key,0,16);
 	cout<<"\n\tChosen key is ";
-	puts(key);
+	for(i=0;i<16;i++)
+		cout<<hex<<(int)key[i]<<" ";
 	cout<<"\n\tMsg is ";
 	memset(msg,0,16);
 	for(i=0;i<16;i++)
 		cout<<hex<<(int)msg[i];
 	keySched(key,S);
-	RC6(crypt,msg,S);
-	cout<<"\n\tEncrypted Msg is ";
+	cout<<"\n\tKeys are";
+	for(i=0;i<2*r+4;i++)
+		cout<<"\n\t"<<dec<<i+1<<" "<<hex<<S[i];
+	RC6Encrypt(crypt,msg,S);
+	cout<<"\n\tEncrypted Msg is \n\t";
 	for(i=0;i<16;i++)
-		cout<<endl<<hex<<(int)(unsigned char)crypt[i];
+		cout<<hex<<(int)(unsigned char)crypt[i]<<" ";
+	cout<<"\n\tMsg is \n\t";
+	RC6Decrypt(crypt,msg,S);
+	for(i=0;i<16;i++)
+		cout<<hex<<(int)(unsigned char)msg[i]<<" ";
 	getch();
 }

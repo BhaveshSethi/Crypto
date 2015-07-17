37a1164218c114f7bf04f851006bbf6ddcbe429c
BhaveshSethi <bhaveshsethi1611@gmail.com>
2015-07-17 18:21:51 +0530

    Tables

--- update.bat
+++ update.bat
@@ -3,6 +3,9 @@ cp ../Des.cpp Des.cpp
 cp ../AES.cpp AES.cpp
 cp ../RSA.cpp RSA.cpp
 cp ../RC6.cpp RC6.cpp
+cp ../../include/AEStable.h AEStable.h
+cp ../../include/BFtable.h BFtable.h
+cp ../../include/DEStable.h DEStable.h
 git add .
 git commit -m "$1"
 git push -u origin master
